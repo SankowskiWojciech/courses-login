33adc90d4c4cdf7f902bfbd32f467eed5e82029d
WojciechS <sankowski.wojciech@gmail.com>
2020-08-12 11:24:00 +0200

    removed token validation

--- src/main/java/com/github/sankowskiwojciech/courseslogin/model/tokenvalidation/TokenValidationResult.java
+++
@@ -1,6 +0,0 @@
-package com.github.sankowskiwojciech.courseslogin.model.tokenvalidation;
-
-public enum TokenValidationResult {
-    VALID,
-    INVALID
-}
--- src/main/java/com/github/sankowskiwojciech/courseslogin/service/tokenvalidation/TokenValidationService.java
+++
@@ -1,8 +0,0 @@
-package com.github.sankowskiwojciech.courseslogin.service.tokenvalidation;
-
-import com.github.sankowskiwojciech.courseslogin.model.tokenvalidation.TokenValidationInput;
-
-public interface TokenValidationService {
-
-    void validateToken(TokenValidationInput tokenValidationInput);
-}
